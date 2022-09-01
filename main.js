8fecac8961cc4a03e25e3489a140a36dc02fceb4
“André <andre_ranulfo@hotmail.com>
2022-09-01 20:03:18 -0300

    FinishBT and No Items message working

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+var items = 0;
+if (items != 0) {
+    noItemVisibility('hidden')
+} else {
+    console.log("-" + items)
+}
+
+
+
 function add() {
-    
+    noItemVisibility('hidden')
+
     //Each div has its own id e.g: id_4, id_5, etc.
     let i = divCount()
     let id = 'id_' + i
@@ -10,19 +20,19 @@ function add() {
     newDiv.setAttribute("class", "item")
 
     //Create an input text
-    var newInput = document.createElement("input")
+    let newInput = document.createElement("input")
     newInput.setAttribute("type", "text")
     newInput.setAttribute("class", 'newInput form-control')
 
     //Create an input button + 
-    var newAdd = document.createElement("button")
+    let newAdd = document.createElement("button")
     newAdd.setAttribute("type", "button")
     newAdd.setAttribute("onclick", 'add()')
     newAdd.setAttribute("class", "btn btn-success me-2")
     newAdd.innerHTML = "+"
 
     //Create an input button -
-    var newBT = document.createElement("button")
+    let newBT = document.createElement("button")
     newBT.setAttribute("type", "button")
     newBT.setAttribute("onclick", 'deleteDiv(' + i + ')')
     newBT.setAttribute("class", "btn btn-danger")
@@ -34,6 +44,11 @@ function add() {
     newDiv.appendChild(newInput)
     newDiv.appendChild(newAdd)
     newDiv.appendChild(newBT)
+    items++;
+    finishBT()
+    finishBTvisibility('visible')
+
+
 }
 
 
@@ -43,9 +58,62 @@ function deleteDiv(i) {
     let id = "id_" + i
     toBeDeleted = document.getElementById(id)
     toBeDeleted.remove()
+    items--;
+    if (items == 0) {
+        noItemVisibility('visible')
+        finishBTvisibility('hidden')
+
+    }
+    finishBT()
 
 }
 
 function divCount() {
     return document.querySelectorAll('.item').length;
+}
+
+// add finish button
+
+function finishBT() {
+
+    let finishBTCount = document.querySelectorAll(".finish").length;
+
+    if (finishBTCount != 0) {
+        return
+    }
+
+    if (items == 1) {
+
+        //Create an input button 
+        let finishBT = document.createElement("button")
+        finishBT.setAttribute("type", "button")
+        finishBT.setAttribute("onclick", 'finish()')
+        finishBT.setAttribute("class", "finish btn btn-secondary me-2 mt-2 ms-3")
+        finishBT.innerHTML = "Finish"
+
+        document.getElementById('finishBT').appendChild(finishBT)
+    } else {
+        return
+    }
+
+}
+
+function noItemVisibility(status) {
+
+
+    document.getElementById("noItem").style.visibility = status;
+
+}
+
+function finishBTvisibility(status) {
+
+
+    document.getElementById("finishBT").style.visibility = status;
+
+}
+
+
+function startItems() {
+    items = 0;
+
 }
